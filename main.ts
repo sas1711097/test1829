70905afccaffcc4bd6ccc050c8c8e6264fdce275
sas1711097 <104481004+sas1711097@users.noreply.github.com>
2022-06-09 11:39:00 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,16 +5,32 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     )))
-    basic.showNumber(sonar.ping(
+    if (sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
     PingUnit.Centimeters
-    ))
+    ) > 2) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # . # .
+            . # # # .
+            . # . # .
+            `)
+    } else {
+        basic.showLeds(`
+            . # # . .
+            . # . # .
+            . # # . .
+            . # . # .
+            . # # . .
+            `)
+    }
 })
 basic.forever(function () {
     music.playTone(262, music.beat(BeatFraction.Half))
     basic.pause(500)
     music.playTone(294, music.beat(BeatFraction.Half))
-    basic.pause(100)
+    basic.pause(200)
     music.playTone(330, music.beat(BeatFraction.Half))
 })
